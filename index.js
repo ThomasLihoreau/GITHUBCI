ecb7fe442b85fb7765d66dd176e4bb34e82b8fa7
Thomas Lihoreau <thomas.lihoreau@epitech.eu>
2021-02-02 18:50:50 +0100

    [FIX] pretty style

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const router = require('./routes/MainRoutes');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const router = require("./routes/MainRoutes");
 const port = 42069 || process.env.PORT;
 const app = express();
 
+app.use(cors());
 
-
-app.use(cors())
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use('/', router);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", router);
 
 app.listen(port, () => {
-    console.log(`[✅] -- Server listening on http://localhost:${port} Have fun! 😊`)
+  console.log(`[✅] -- Server listening on http://localhost:${port} Have fun! 😊`);
 });
--- routes/MainRoutes.js
+++ routes/MainRoutes.js
@@ -3,68 +3,55 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 
 Router.use((req, res, next) => {
-	const date = new Date();
-	console.log(
-		`[🕔] -- Caught @ ${date.getHours()}:${date.getMinutes()} -> ${
-			req.ip
-		} on ${req.url}`
-	);
-	next();
+  const date = new Date();
+  console.log(`[🕔] -- Caught @ ${date.getHours()}:${date.getMinutes()} -> ${req.ip} on ${req.url}`);
+  next();
 });
 
 Router.post("/login", (req, res) => {
-	const goodCredentials = {
-		username: "aled",
-		password: "workshop",
-	};
-	if (!req.body || !req.body.username || !req.body.password) {
-		res.status(400).send({ status: 400, message: "Bad request" });
-	} else if (
-		req.body.username === goodCredentials.username &&
-		req.body.password === goodCredentials.password
-	) {
-		res.status(200).send({ status: 200, message: "Ok." });
-	} else {
-		res.status(401).send({ status: 400, message: "Unauthorized" });
-		console.log(`[✅] -- Logged in!`);
-	}
+  const goodCredentials = {
+    username: "aled",
+    password: "workshop",
+  };
+  if (!req.body || !req.body.username || !req.body.password) {
+    res.status(400).send({ status: 400, message: "Bad request" });
+  } else if (req.body.username === goodCredentials.username && req.body.password === goodCredentials.password) {
+    res.status(200).send({ status: 200, message: "Ok." });
+  } else {
+    res.status(401).send({ status: 400, message: "Unauthorized" });
+    console.log(`[✅] -- Logged in!`);
+  }
 });
 
 Router.post("/register", (req, res) => {
-	if (
-		!req.body ||
-		!req.body.email ||
-		!req.body.username ||
-		!req.body.password
-	)
-		res.status(400).send({ status: 400, message: "Bad request" });
-	else {
-		res.status(200).send({ status: 200, message: "OK." });
-		console.log(`[✅] -- New user registered`);
-	}
+  if (!req.body || !req.body.email || !req.body.username || !req.body.password) res.status(400).send({ status: 400, message: "Bad request" });
+  else {
+    res.status(200).send({ status: 200, message: "OK." });
+    console.log(`[✅] -- New user registered`);
+  }
 });
 
 Router.get("/pupper", (req, res) => {
-	const pupPath = __dirname + "/../public/img/shibe.jpg";
-	console.log(pupPath);
-	if (!fs.existsSync(pupPath))
-		res.status(404).send({
-			status: 404,
-			message: "Pupper not found. :(",
-		});
-	else {
-		res.status(200).download(pupPath);
-	}
+  const pupPath = __dirname + "/../public/img/shibe.jpg";
+  console.log(pupPath);
+  if (!fs.existsSync(pupPath))
+    res.status(404).send({
+      status: 404,
+      message: "Pupper not found. :(",
+    });
+  else {
+    res.status(200).download(pupPath);
+  }
 });
 
 Router.get("/teapot", (req, res) => {
-	res.status(418).send({ status: 418, message: "I'm a teapot." });
-	console.log("[🫖] -- I'm a teapot dude");
+  res.status(418).send({ status: 418, message: "I'm a teapot." });
+  console.log("[🫖] -- I'm a teapot dude");
 });
 
 Router.get("/ping", (req, res) => {
-	res.status(200).send({ status: 200, message: "Pong!" });
-	console.log(`[✅] -- Pong lmao`);
+  res.status(200).send({ status: 200, message: "Pong!" });
+  console.log(`[✅] -- Pong lmao`);
 });
 
 module.exports = Router;
